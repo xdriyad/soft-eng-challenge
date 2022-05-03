76c87488fd225e23315615d750e2aaf86ba66f1e
Omar Faruk Riyad <omar@mixrank.com>
2022-05-03 21:42:25 +0600

    Add api app and services for api

---
+++ api/models.py
@@ -0,0 +1,53 @@
+from django.core.exceptions import ValidationError
+from django.db import models
+import names
+# Create your models here.
+from django.utils import timezone
+
+
+class BaseModel(models.Model):
+    created_at = models.DateTimeField(db_index=True, default=timezone.now)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+class MotherShip(BaseModel):
+    capacity = models.IntegerField(default=9)
+
+    def __str__(self):
+        return str(self.id)
+
+    @property
+    def has_vacancy(self):
+        return self.capacity > Ship.objects.filter(mother_ship=self).count()
+
+
+class Ship(BaseModel):
+    capacity = models.IntegerField(default=5)
+    mother_ship = models.ForeignKey(MotherShip, on_delete=models.CASCADE)
+
+    def clean(self):
+        if not self.mother_ship.has_vacancy:
+            raise ValidationError("Cannot create a ship, the MotherShip has no capacity")
+
+    @property
+    def has_vacancy(self):
+        return self.capacity > Crew.objects.filter(ship=self.id).count()
+
+    def __str__(self):
+        return str(self.id)
+
+
+class Crew(BaseModel):
+    ship = models.ForeignKey(Ship, on_delete=models.CASCADE)
+    name = models.CharField(default=names.get_first_name, max_length=128)
+
+    def clean(self):
+        if not self.ship.has_vacancy:
+            raise ValidationError("Cannot create a crew, ship has no capacity")
+
+    def __str__(self):
+        return '{} : {}'.format(self.id, self.name)
+
+
---
+++ api/services.py
@@ -0,0 +1,22 @@
+from api.models import MotherShip, Ship, Crew
+
+
+def create_mother_ship():
+    mother_ship = MotherShip.objects.create()
+    mother_ship.save()
+    for i in range(3):
+        create_ship(mother_ship=mother_ship)
+
+
+def create_ship(mother_ship):
+    ship = Ship.objects.create(mother_ship=mother_ship)
+    if ship.clean():
+        ship.save()
+        for i in range(3):
+            create_crew(ship=ship)
+
+
+def create_crew(ship):
+    crew = Crew.objects.create(ship=ship)
+    if crew.clean():
+        crew.save()
