7d9313c2abd8dd705d3d04a34bae28cec56306a1
Omar Faruk Riyad <omar@mixrank.com>
2022-05-06 17:23:43 +0600

    Fix workflow and test dependencies

--- airforce/settings.py
+++ airforce/settings.py
@@ -9,8 +9,15 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
+from confidential import SecretsManager
+
+secret_file = Path.joinpath(Path(__file__).resolve().parent.parent, '.confidential/default.json')
+confidential = SecretsManager(
+    secrets_file_default=secret_file,
+    region_name="us-east-1"
+)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +27,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-h1h%@eru_@njnlt7*u!u62)g^@f!i7av=lvsm6bku)9fs^d$+w'
+SECRET_KEY = confidential['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = confidential["DEBUG"]
 
 ALLOWED_HOSTS = []
 
