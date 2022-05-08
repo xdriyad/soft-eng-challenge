26343500939d63173fab83d46850d8d77d91e90c
Omar Faruk Riyad <omar@mixrank.com>
2022-05-09 02:21:07 +0600

    isort and change hard coded status codes

--- api/tests.py
+++ api/tests.py
@@ -1,5 +1,6 @@
 import names
 from django.urls import reverse
+from rest_framework import status
 from rest_framework.test import APITestCase
 
 
@@ -14,24 +15,24 @@ class TestApi(APITestCase):
 
     def test_create_mothership(self):
         response = self.client.post(reverse('mothership_list'))
-        self.assertEqual(response.status_code, 201)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
     def test_default_ship_creation(self):
         self.assertEqual(len(self.mothership['ships']), 3)
 
     def test_create_ship(self):
         response = self.client.post(reverse('ship_list'), {'mothership': self.mothership['id']})
-        self.assertEqual(response.status_code, 201)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
     def test_create_ship_with_count(self):
         response = self.client.post(reverse('ship_list'), {'mothership': self.mothership['id'], 'count': 2})
-        self.assertEqual(response.status_code, 201)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertEqual(len(response.data), 2)
 
     def test_create_crew(self):
         ship = self.mothership['ships'][0]
         response = self.client.post(reverse('crew_list'), {'name': names.get_first_name(), 'ship': ship['id']})
-        self.assertEqual(response.status_code, 201)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
     def test_default_create_crew(self):
         ship = self.mothership['ships'][0]
@@ -42,7 +43,7 @@ class TestApi(APITestCase):
         ship = self.mothership['ships'][0]
         for i in range(3):
             response = self.client.post(reverse('crew_list'), {'name': names.get_first_name(), 'ship': ship['id']})
-        self.assertEqual(response.status_code, 405)
+        self.assertEqual(response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
         ship = self.client.get(reverse('ship_details', kwargs={'pk': ship['id']})).data
         self.assertEqual(len(ship['crew']), 5)
 
